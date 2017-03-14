7472e430dc51cda06c5a0b2da21b1ed8b2556d48
cpsavva <csoriasavva@gmail.com>
2017-03-13 22:01:23 -0400

    adding copied files from burger repo

---
+++ database/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+
+	id INT(4) AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(200) NOT NULL,
+	devoured BOOLEAN NOT NULL,
+	eaten TIMESTAMP,
+	PRIMARY KEY (id)
+);
+
---
+++ database/seeds.sql
@@ -0,0 +1,10 @@
+/*inserting preliminary entries to database */
+
+INSERT INTO burgers (burger_name, devoured) 
+	VALUES ('cheeseburger', true);
+	
+INSERT INTO burgers (burger_name, devoured) 
+	VALUES ('wyoming', false);
+
+INSERT INTO burgers (burger_name, devoured)	
+	VALUES ('copsegmore', false);
